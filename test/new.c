9087a1165130cda16d6c2a5dfa4b46404defdf9c
kimsj5025 <kimsj5025@naver.com>
2021-04-17 15:45:45 +0900

    쉬프트로 2배

--- test/new.c
+++ test/new.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <windows.h>
 int main() {
-	int a,b;
+	int a, b;
 	scanf("%d %d", &a, &b);
-	printf("%d\n%d\n%d\n%d\n%d\n",  a+b, a-b, a*b, a/b, a%b);
+	printf("%d", a << 1);
 }
