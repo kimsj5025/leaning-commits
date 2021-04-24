fb2ce97e1f0b82a4e219c52f8164e2fe49d52a54
kimsj5025 <kimsj5025@naver.com>
2021-04-24 13:44:08 +0900

    정수 받으면 더해서 출력

--- test/new.c
+++ test/new.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <windows.h>
 int main() {
-	system("title 구구단 계산기");
-	system("mode con:cols=50 lines=30");
 
-
-	int i = 0, eks;
-	scanf("%d", &eks);
-	while (i < 9) {
-		
-
-		printf("%d * %d = %d\n", eks, i + 1, eks * (1 + i));
-		i++;
+	start();
+	
+	if (MessageBox(NULL, TEXT("Are you run progrme?"), TEXT("Are you sure?"), MB_YESNO) == 7) { 
+		return; 
 	};
-	system("pause");
 
+
+	int i = 0, eks, i2;
+	while (1) {
+		system("cls");
+		i = 1, eks = 0, i2 = 0;
+		printf("정수를 입력하세요 : ");
+		scanf("%d", &eks);
+		while (i <= eks) {
+
+			i2 = i2 + i;
+			i++;
+		};
+		printf("입력된 수 까지의 합 : %d\n\n", i2);
+		system("pause");
+	};
+
+
+}
+
+
+int start() {
+	system("title 입력된 수 까지의 합 구하기");
+	system("mode con:cols=35 lines=20");
 }
