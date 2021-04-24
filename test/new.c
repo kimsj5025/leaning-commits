93cc864b3d74a5edb0bc48ef0af18d033c0812e5
kimsj5025 <kimsj5025@naver.com>
2021-04-24 10:24:54 +0900

    2전

--- test/new.c
+++ test/new.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <windows.h>
 int main() {
-	int s;
-	float x = 1.3, y = 1.8;
-	s = x + y;
-	printf("%d\n", s);
-	s = (int)x + (int)y;
-	printf("%d\n", s);
+	printf("%d\n", 4 / 3);
+	printf("%f\n", 4 / 3);
+	printf("%f\n",(float)4 / 3);
+	printf("%f\n", 4 / 3.0);
 }
