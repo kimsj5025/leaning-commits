43038053b20c249c0d6c80d58497f364fc66726b
kimsj5025 <kimsj5025@naver.com>
2021-04-24 13:14:36 +0900

    아니 ㅋㅋ 9가 아니라 99로함

--- test/new.c
+++ test/new.c
@@ -2,11 +2,12 @@
 #include <windows.h>
 int main() {
 	system("title 구구단 계산기");
+	system("mode con:cols=50 lines=30");
 
 
 	int i = 0, eks;
 	scanf("%d", &eks);
-	while (i < 99) {
+	while (i < 9) {
 		
 
 		printf("%d * %d = %d\n", eks, i + 1, eks * (1 + i));
