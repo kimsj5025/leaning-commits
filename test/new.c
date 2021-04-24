72d961dcaa0522084806ff4b25ac834a0caadff1
kimsj5025 <kimsj5025@naver.com>
2021-04-24 11:15:24 +0900

    과목별 점수 계산 프로그램

--- test/new.c
+++ test/new.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <windows.h>
 int main() {
-	int a, b;
-	a = 4 * (1/2);//int가 정수형이라서 1/2는 0이 되고 4*0 = 0이 된다
-	b = a++;
-	printf("%d\n%d", a, b);
+	float mid, end, trash,var;
+
+	printf("** 과목별 점수 계산 프로그램 **\n\n");
+
+	printf("\n중간고사 반영비율/받은점수 : ");
+	scanf("%f %f", &var, &mid);
+	mid = mid * var;
+
+	printf("\n기말고사 반영비율/받은점수 :");
+	scanf("%f %f", &var, &end);
+	end = end * var;
+
+	printf("\n수행평가 반영비율/받은점수 :");
+	scanf("%f %f", &var, &trash);
+	trash = trash * var;
+
+	printf("\n총 점수 : %0.1f", end + trash + mid);
+
 }
-//???????????????????????
