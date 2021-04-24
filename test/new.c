a523a482214559201ddb6fdce114167e6f6ca491
kimsj5025 <kimsj5025@naver.com>
2021-04-24 11:52:57 +0900

    점수계산 프로그램.파이날

--- test/new.c
+++ test/new.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <windows.h>
 int main() {
+
 	float mid, end, trash,var;
 
 	printf("** 과목별 점수 계산 프로그램 **\n\n");
@@ -9,14 +10,16 @@ int main() {
 	scanf("%f %f", &var, &mid);
 	mid = mid * var;
 
-	printf("\n기말고사 반영비율/받은점수 :");
+	printf("\n기말고사 반영비율/받은점수 : ");
 	scanf("%f %f", &var, &end);
 	end = end * var;
 
-	printf("\n수행평가 반영비율/받은점수 :");
+	printf("\n수행평가 반영비율/받은점수 : ");
 	scanf("%f %f", &var, &trash);
 	trash = trash * var;
 
-	printf("\n총 점수 : %0.1f", end + trash + mid);
+	printf("\n총 점수 : %0.1f\n\n\n", end + trash + mid);
+
+	system("pause");
 
 }
