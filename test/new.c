b30680aaa927d0d639c66e364c4d8ddaa4ad67c7
kimsj5025 <kimsj5025@naver.com>
2021-04-24 15:07:19 +0900

    매우 친절하게 만듬

--- test/new.c
+++ test/new.c
@@ -18,12 +18,12 @@ int main() {
 		system("cls");
 
 		i = 1, n = 0, i2 = 0;
-
+		printf("\(매우 친절한 말투로\)1이상의 정수를 입력해주세요 : ");
 		scanf("%d", &n);
 
 		system("cls");
 		do{
-
+			printf("\(매우 친절한 말투로\)출력 시작 : ");
 			i2 = 3 * i;
 			printf("%d\n", i2);
 			i++;
