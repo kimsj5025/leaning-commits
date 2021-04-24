f0e001867e2bd107c23ed13f83ab28dd67788dd5
kimsj5025 <kimsj5025@naver.com>
2021-04-24 15:05:30 +0900

    3의 배수 n개 출력 (날먹)

--- test/new.c
+++ test/new.c
@@ -2,30 +2,41 @@
 #include <windows.h>
 
 
-float i = 0, n, i2, i3;
+int i = 0, n, i2;
 
 
 int main() {
 
 	start();
-	
-	if (MessageBox(NULL, TEXT("Are you run progrem ?"), TEXT("Are you sure?"), MB_YESNO) == 7) { 
-		return; 
+
+	if (MessageBox(NULL, TEXT("Are you run progrme?"), TEXT("Are you sure?"), MB_YESNO) == 7) {
+		return;
 	};
 
-	
-	printf("1이상의 수 입력  : ");
-	scanf("%f", &n);
-	do {
-		printf("hello world\n");
-		i++;
-	} while (i <= n);
-	system("pause");
+	while (1) {
 
-}  
+		system("cls");
+
+		i = 1, n = 0, i2 = 0;
+
+		scanf("%d", &n);
+
+		system("cls");
+		do{
+
+			i2 = 3 * i;
+			printf("%d\n", i2);
+			i++;
+
+		}while (i <= n);
+		system("pause");
+	};
+
+
+}
 
 
 int start() {
-	system("title 0을 입력할 때까지 정수받아 더해 출력하기");
-	//system("mode con:cols=20 lines=20");
+	system("title 3위 배수 n번 출력하기");
+	//system("mode con:cols=30 lines=20");
 }
