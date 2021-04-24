31f57ca483c834978cd7c623a0fedf2eb0c5a0bc
kimsj5025 <kimsj5025@naver.com>
2021-04-24 10:39:03 +0900

    3번
    ㅋㅋㅋㅋㅋㅋㅋㅋㅋ

--- test/new.c
+++ test/new.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <windows.h>
 int main() {
-	printf("%d\n", 4 / 3);
-	printf("%f\n", 4 / 3);
-	printf("%f\n",(float)4 / 3);
-	printf("%f\n", 4 / 3.0);
+	int a, b;
+	a = 4 * (1/2);//int가 정수형이라서 1/2는 0이 되고 4*0 = 0이 된다
+	b = a++;
+	printf("%d\n%d", a, b);
 }
+//???????????????????????
