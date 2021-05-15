cffa94654ee791661e6cd02a14b06e887ef42be7
kimsj5025 <kimsj5025@naver.com>
2021-05-15 14:15:40 +0900

    중첩 while 연습문제 2

--- test/new.c
+++ test/new.c
@@ -6,17 +6,19 @@ int main() {
 
 	int i = 0, eks=1, i2=0;
 
-	while (i2 < 8) {
-		eks++;
+	while (i2 < 5) {
+	
+		printf("0보다 큰 수를 입력하세요(%d 번째) : ",i2+1);
+		scanf("%d", &eks);
+		while (eks > 0) {
 		i2++;
-		i = 0;
-		while (i < 9) {
-			printf("%d * %d = %d\n", eks, i + 1, eks * (1 + i));
-			i++;
-		};
-		printf("\n");
+		i=i + eks;
+		eks = -1;
+		}
+	
 
 	};
+	printf("\n\ntotal : %d\n\n", i);
 	system("pause");
 
 }
