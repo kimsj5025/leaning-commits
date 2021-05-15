704eda0d546067a4c423a75891a091457db2c110
kimsj5025 <kimsj5025@naver.com>
2021-05-15 13:29:01 +0900

    실전문제 1

--- test/new.c
+++ test/new.c
@@ -7,10 +7,18 @@ int i, n, i2, i3, a, b, c;
 
 int main() {
 	
-	scanf("%d", &a);
+	scanf_s("%d", &a);
 
 	for (i = 0; i <= a; i++) {
-		for ( i2 = 0; i2 + i < a; i2++)
+		for (i2 = 0; i2 + i < a; i2++)
+		{
+			printf(" ");
+		}
+		for (i2 = 0; i2 <= i; i2++)
+		{
+			printf("*");
+		}
+		for (i3 = 0; i3 < i; i3++)
 		{
 			printf("*");
 		}
