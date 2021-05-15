504daa4684d8c01a2b31c3a07d25dd7e345f2755
kimsj5025 <kimsj5025@naver.com>
2021-05-15 15:06:11 +0900

    큰수 - 작은수

--- test/new.c
+++ test/new.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <windows.h>
+
+
+int a, b, c;
 int main() {
-	system("title 구구단 계산기");
 
-
-	int i = 0, eks=1, i2=0;
-
-	while (i < 5) {
-		i2 = i;
-		while (i2 > 0) {
-			printf("o");
-			i2--;
-		}
-		printf("*\n");
-		i++;
+	scanf("%d %d", &a,&b);
+	if (a < b) {
+		printf("%d\n", b - a);
+	}
+	else {
+		printf("%d\n", a - b);
 	}
 
-	system("pause");
 
+	printf("종료\n");
+
+	system("pause");
 }
