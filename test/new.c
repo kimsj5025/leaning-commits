5fa62f74ef23feda1e0f03d838831ca48f70300c
kimsj5025 <kimsj5025@naver.com>
2021-07-24 14:44:56 +0900

    2차원 배열

--- test/new.c
+++ test/new.c
@@ -2,22 +2,20 @@
 #include <windows.h>
 
 int main() {
-
-	int a=1, b=2;
-	switchNum(&a,&b);
-
-	printf("%d %d\n", a, b);
-
-	system("pause");
-
-	return 0;
-
-}
-
-int switchNum(int *a,int *b) {
-	int i;
-	i = *a;
-	*a = *b;
-	*b = i;
 	
+	char a[2][4] = {
+		{1,2,3,4},
+		{5,6,7,8}
+
+	};
+	for ( int i = 0; i <2; i++)
+	{
+		for  (int i2 = 0; i2 <4; i2++)
+		{
+		printf("%d", a[i][i2]);
+		}
+		printf("\n");
+	}
+	system("pause");
+	return 0;
 }
