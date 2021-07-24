5f208c5a322ea2c3160972f1479b3dae4be4265a
kimsj5025 <kimsj5025@naver.com>
2021-07-24 14:52:55 +0900

    Array01

--- test/new.c
+++ test/new.c
@@ -3,19 +3,26 @@
 
 int main() {
 	
-	char a[2][4] = {
-		{1,2,3,4},
-		{5,6,7,8}
+	int a[3][9];
 
-	};
-	for ( int i = 0; i <2; i++)
+	for ( int i = 0; i <3; i++)
 	{
-		for  (int i2 = 0; i2 <4; i2++)
+		for  (int i2 = 0; i2 <9; i2++)
 		{
-		printf("%d", a[i][i2]);
+			a[i][i2] = (i + 5) * (i2+1);
 		}
 		printf("\n");
 	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int i2 = 0; i2 < 9; i2++)
+		{
+			printf("%d ", a[i][i2]);
+		}
+		printf("\n");
+	}
+
 	system("pause");
 	return 0;
 }
